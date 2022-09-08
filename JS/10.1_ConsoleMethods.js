341d7ca820c0539d161da6947b2f122e9972650f
KushalThapa <kushalmagar007@gmail.com>
2022-09-08 20:57:20 +0545

    added dom

---
+++ JS/10.1_ConsoleMethods.js
@@ -0,0 +1,19 @@
+obj = {a:2, b:3, c:4}
+console.table(obj)
+
+console.clear() // clear all values in the log
+
+console.warn("Hey please don't dreank suger") // when you have to show warning
+console.info("Hey this is an important info")// as similar as console.log 
+console.warn("This is an error")// shows in red text 
+
+console.assert(3>45)// when you write tru fact it shows true but and if you enter false statement it shows error
+
+console.time()
+console.time("a")
+console.timeEnd("a")
+console.log("forLoop")
+
+for(let i=0;i<5;i++){
+console.log(233)
+}
